4bfe4e8b844a7bc59b747ab9b96e68f8c64e0afe
Sam La <Sla@wpi.edu>
2017-07-24 23:34:12 -0400

    added loading screen for now

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      r: 0,
+      g: 255,
+      b: 0
+    }
+  }
+
+  changeColors = () => {
+    this.setState ({
+      r: Math.floor(Math.random() * 255) + 1,
+      g: Math.floor(Math.random() * 255) + 1,
+      b: Math.floor(Math.random() * 255) + 1
+    });
+  }
+
   render() {
+    const { r, g, b } = this.state
+
+    setTimeout(() => {
+      this.changeColors()
+    }, 500)
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container" style={{color: `rgb(${r},${g},${b})`}}>
+        I am not dating food
       </div>
     );
   }
